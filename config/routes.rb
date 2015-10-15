8669285882542e41e80b7ea664e89b945a496f1d
Andrea Ros <andrea@donboscoland.it>
2015-10-15 16:22:20 +0200

    Validazione

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :products
 
-  # prova 2
+  # prova 2 3
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
